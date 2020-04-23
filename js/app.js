b8948a0430d4bde3c9dc8afef263dda444184f08
Shannon Lounges <shannonlounges@Shannons-MacBook-Pro.local>
2020-04-23 18:08:32 -0400

    updating js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
-new Chart(document.getElementById('barChart'), {
-	type: 'bar',
-	data: {
-		labels: ['Gold', 'Silver', 'Bronze']
-		datasets: [ 
-			{
+new Chart (document.getElementById('barChart'), {
+		type: 'bar',
+		data:  {
+			labels: ['Gold', 'Silver', 'Bronze']
+			datasets: [
+				{
+				    labels:'Canadian Gold Medals (Won)',
+					data: ['315', '203', '107'],
+					backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f'],
+				}
 
-				labels:'Canadian Gold Medals (Won)',
-				data: ['315', '203', '107'],
-			}
-		]
-	}
+			]
+		}
+
+
+
+
 });
