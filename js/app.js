c8ba0efbdaef9c3d1c808c2f15539c6cf633445c
Shannon Lounges <shannonlounges@Shannons-MacBook-Pro.local>
2020-04-23 21:20:44 -0400

    changes to js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,57 @@
 new Chart (document.getElementById('barChart'), {
 		type: 'bar',
 		data:  {
-			labels: ['Gold', 'Silver', 'Bronze']
+			labels: ['Gold', 'Silver', 'Bronze'],
 			datasets: [
 				{
-				    labels:'Canadian Gold Medals (Won)',
+				    label:'Gold Medals (hundreds)',
 					data: ['315', '203', '107'],
 					backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f'],
 				}
 
 			]
+		},
+		options: {
+			legend: {
+				display: false
+			}
 		}
 
+});
+new Chart (document.getElementById('barCharttwo'), {
+		type: 'bar',
+		data:  {
+			labels: ['Men', 'Women'],
+			datasets: [
+				{
+				    label:'Gold Medals (hundreds)',
+					data: ['386', '230'],
+					backgroundColor: ['#FF8B4B', '#45EFF5'],
+				}
+
+			]
+		},
+		options: {
+			legend: {
+				display: false
+			}
+		}
+
+});
+new Chart(document.getElementById('lineChart'),{
+	type: 'line',
+	data: {
+		labels: ['Biathlon', 'Bobsleigh', 'Curling', 'Ice Hockey','Skating', 'Skiing'],
+		datasets: [
+			{
+				label: 'Canadian Medals',
+				data: ['3', '22', '50', '351', '179', '40'],
+				fill: false,
+				borderColor: '#3e95cd',
+			}
 
 
+		]
+	}
 
 });
